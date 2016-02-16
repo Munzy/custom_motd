f26daf093977bc2ed1b02c38ecfc35e9f59447c4
Cameron Munroe <cmunroe@cameronmunroe.com>
2016-02-15 20:38:27 -0800

    Installers.

---
+++ inst/centos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+yum install gcc make -y
+cd /tmp
+wget http://humdi.net/vnstat/vnstat-1.12.tar.gz
+tar xzf vnstat-1.12.tar.gz
+cd /tmp/vnstat-1.12
+
+make 
+make install
+cp /tmp/vnstat-1.12/examples/init.d/redhat/vnstat /etc/init.d/vnstat
+chmod +x /etc/init.d/vnstat
+/etc/init.d/vnstat start
+cd /etc/init.d/
+chkconfig --add vnstat
+chkconfig vnstat on
+
+cd /usr/bin/
+wget https://git.enjen.net/Munzy/custom_motd/raw/master/custom_motd
+chmod +x custom_motd
+
+echo 'custom_motd' >> /etc/profile
---
+++ inst/deb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update 
+apt-get install ca-certificates -y
+apt-get install vnstat -y
+apt-get install wget -y
+
+
+cd /usr/bin/
+wget https://git.enjen.net/Munzy/custom_motd/raw/master/custom_motd
+chmod +x custom_motd
+
+echo 'custom_motd' >> /etc/profile
