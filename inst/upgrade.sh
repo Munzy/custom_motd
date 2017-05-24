bd491dd183a18c4c25dd7c902bd8d2173803971a
Munzy <munroenet@gmail.com>
2017-05-23 18:32:32 -0700

    Update upgrade.sh

--- inst/upgrade.sh
+++ inst/upgrade.sh
@@ -1,4 +1,4 @@
 cd /usr/bin/
 rm custom_motd
-wget https://raw.githubusercontent.com/Munroenet/custom_motd/master/custom_motd
+wget https://raw.githubusercontent.com/Munzy/custom_motd/master/custom_motd
 chmod +x custom_motd
